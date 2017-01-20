cc1e0996f610faff8f3a91084a535a29e637620c
RunF0rrestRun <sonyanderson19@gmail.com>
2017-01-20 15:46:13 -0300

    Layout complete
    ...

---
+++ BalancoTrimestral.java
@@ -0,0 +1,9 @@
+class{
+	public static void(String[] args){
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		int gastosTrimestrais = gastosJaneiro + gastosFevereiro + gastosMarco;
+		System.out.print(gastosTrimestrais);
+	}
+}
