4862c8f4fb4f7bd87088defa84d44edef17e3d73
RunF0rrestRun <sonyanderson19@gmail.com>
2017-01-27 00:47:22 -0300

    Control Pane (in constructionn
    ...

---
+++ js/controller/gpr_ctrl.js
@@ -0,0 +1,46 @@
+angular.module('gpr').controller('gprCtrl', function($scope){
+	
+	$scope.usuarios = [
+	{name:"Anderson", email:"jony@gmail.com" ,login:"jony", password:"123" ,type:"Admin"},
+	{name:"Paulo", email:"paulo@gmail.com" ,login:"paulo", password:"123" ,type:"Admin"},
+	{name:"Kecia", email:"kecia@gmail.com" ,login:"kecia", password:"123" ,type:"Cliente"},
+	{name:"Manoel", email:"manoel@gmail.com" ,login:"manoel", password:"123" ,type:"Tecnico"}
+	];
+
+
+	$scope.home = true;
+	$scope.viewSite = true;
+
+	$scope.doLogin = function(contato){
+		if(contato.login == usuarios[0].login && contato.password == usuarios[0].password){
+			$scope.viewSite = false;
+			$scope.viewPanel = true;
+		}else{
+			alert("Login ou senha incorretos");
+		}
+	};
+	$scope.viewHome = function(){
+		$scope.criarCadastro = false;
+		$scope.abrirChamado = false;
+		$scope.home = true;
+		$scope.followChamado = false;
+	};	
+	$scope.viewCadastro = function(){
+		$scope.criarCadastro = true;
+		$scope.abrirChamado = false;
+		$scope.home = false;
+		$scope.followChamado = false;
+	};
+	$scope.viewChamado = function(){
+		$scope.abrirChamado = true;
+		$scope.criarCadastro = false;
+		$scope.home = false;
+		$scope.followChamado = false;
+	};	
+	$scope.viewAcompanhar = function(){
+		$scope.abrirChamado = false;
+		$scope.criarCadastro = false;
+		$scope.home = false;
+		$scope.followChamado = true;
+	};	
+});
